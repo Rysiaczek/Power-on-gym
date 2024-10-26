0bede58613a84393e16547daf62b6fd58b52bd9b
Ryszard Gruszczyński <90858674+Rysiaczek@users.noreply.github.com>
2024-10-26 16:08:52 +0200

    ss

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -1,10 +1,28 @@
 import "../styles/nav.css";
+import React, { useState } from "react";
 
 function Nav() {
+  const [active, setActive] = useState(false);
+
+  const respons = () => {
+    const links = document.querySelector(".links");
+
+    if (active === false) {
+      links.style.left = "0";
+      setActive(true);
+    } else {
+      links.style.left = "100%";
+      setActive(false);
+    }
+  };
+
   return (
     <div className="nav">
       <div className="logo">
         <img src={process.env.PUBLIC_URL + "/img/logo.png"} alt="logo" />
+      </div>
+      <div className="respons">
+        <i class="bi bi-list" onClick={respons}></i>
       </div>
       <div className="links">
         <a href="#">o nas</a>
